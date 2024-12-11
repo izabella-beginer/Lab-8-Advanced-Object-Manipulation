72a65b4b7ade1247de257a6c9164f79e93eaca8c
Izabella Balogh <n01716105@humber.ca>
2024-12-11 02:35:49 -0500

    body and imageis added to styles.css

---
+++ script.js
@@ -0,0 +1,11 @@
+let student = {
+    name: "Izabella",
+    age: 24,
+    enrolled: true,
+    courses: ['javascript','web dev','algorithm','databse','wrti'],
+    displayinfo: function () {
+    console.log(person.name); // Output: izabella
+    console.log(person.age); // Output: 24
+    console.log(person.courses);
+}
+}
